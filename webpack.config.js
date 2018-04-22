d8ed0e91d438856491ba735dfba17b523ca8b1ee
Nathan Schulzke <nschulzke@gmail.com>
2018-04-22 09:46:40 -0600

    Hotswapping

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './app/app.js',
   mode: 'none',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   output: {
     path: path.resolve(__dirname, 'app/dist'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+    publicPath: '/dist/',
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'app'),
-    publicPath: '/dist',
-    watchContentBase: true,
+    hotOnly: true,
   }
 };
